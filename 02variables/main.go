1aeed6f99d99b92f7ef4f1ba1b3142456f07ffbf
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2023-08-14 23:31:53 +0530

    Initial commit

--- 02variables/main.go
+++ 02variables/main.go
@@ -2,6 +2,49 @@ package main
 
 import "fmt"
 
+// var username string = "shaad" // This will work
+// jwtToken := "asdasdasd" // This will not work
+// var jwtToken = "asdasdasd" // This will work
+
+const LoginToken string = "asdasdasd" // Public variable because it starts with capital letter
+
 func main () {
-	fmt.Println("Variables");
+	var username string = "shaad"
+
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T\n\n", username)
+
+	var isLoggedIn bool = false
+
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T\n\n", isLoggedIn)
+
+	var smallVal uint16 = 256
+
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of type: %T\n\n", smallVal)
+
+	var smallFloat float64 = 256.9230123
+
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type: %T\n\n", smallFloat)
+
+	// Default values and some aliases
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable is of type: %T\n\n", anotherVariable)
+
+	// Implicit type
+
+	var website = "shaad.pro"
+	fmt.Println(website)
+
+	// Shorthand
+	// Only works inside a function
+	// Outside a function, we use var
+	numberOfUser := 300000
+	fmt.Println(numberOfUser)
+
+	fmt.Println(LoginToken)
+	fmt.Printf("Variable is of type: %T\n\n", LoginToken)
 }
